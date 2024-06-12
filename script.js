d3a35cb6116b5863b1e3e62a155ce01ff912f558
jaluiovilash <ovilashjalui@gmail.com>
2024-06-12 16:33:42 +0530

    updated

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 console.log("This is working");
 
 document.querySelector(".btn").addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
     console.log("clicked");
     console.log(username.value, password.value);
 
-    let passwords = localStorage.getItem("passwords")
+    let passwords = localStorage.getItem("passwords");
     console.log(passwords);
 
     if (passwords == null) {
         let json = []
-        json.push = ({ username: username.value, password: password.value })
-        alert("Please fill out the details")
-        localStorage.getItem("passwords", JSON.stringify)
+        json.push = ({ username: username.value, password: password.value });
+        alert("Please fill out the details");
+        localStorage.getItem("passwords", JSON.stringify);
     }
     else {
-        let json = JSON.parse(localStorage.getItem("passwords"))
-        json.push = ({ username: username.value, password: password.value })
-        alert("Password Saved")
-        localStorage.getItem("passwords", JSON.stringify)
+        let json = JSON.parse(localStorage.getItem("passwords"));
+        json.push = ({ username: username.value, password: password.value });
+        alert("Password Saved");
+        localStorage.getItem("passwords", JSON.stringify);
     }
 
 })
